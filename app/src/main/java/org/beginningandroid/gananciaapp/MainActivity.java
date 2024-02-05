9d253c842d39a670dc798469ea7b8351395228ec
jolimo <joseliz.motta@gmail.com>
2024-02-05 14:26:11 -0500

    Initial commit

---
+++ app/src/main/java/org/beginningandroid/gananciaapp/MainActivity.java
@@ -0,0 +1,156 @@
+package org.beginningandroid.gananciaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+ * La clase MainActivity representa la actividad principal de la aplicación.
+ * Esta clase se encarga de manejar las interacciones del usuario con la interfaz de la aplicación.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * EditText para el primer número.
+     */
+    private EditText numero1;
+
+    /**
+     * EditText para el segundo número.
+     */
+    private EditText numero2;
+
+    /**
+     * TextView para mostrar el resultado.
+     */
+    private TextView resultado;
+
+    /**
+     * RadioGroup para seleccionar la opción de cálculo.
+     */
+    private RadioGroup radioGroup;
+
+    /**
+     * DecimalFormat para formatear los números.
+     */
+    private DecimalFormat df = new DecimalFormat("#,###");
+
+    /**
+     * Este método se llama cuando se crea la actividad.
+     * Inicializa la interfaz de usuario y establece los listeners de los eventos.
+     *
+     * @param savedInstanceState Si la actividad se reinicia después de una pausa o "stop", este es el último estado guardado.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Inicializa los componentes de la interfaz de usuario.
+        numero1 = findViewById(R.id.valorCompra);
+        numero2 = findViewById(R.id.ganancia);
+        resultado = findViewById(R.id.resultado);
+        radioGroup = findViewById(R.id.radioGroup);
+
+        // Establece el listener del botón calcular.
+        Button calcular = findViewById(R.id.calcular);
+        calcular.setOnClickListener(this::OnClick);
+
+        // Agrega TextWatchers a los EditText para manejar los cambios de texto.
+        numero1.addTextChangedListener(new NumberTextWatcher(numero1));
+        numero2.addTextChangedListener(new NumberTextWatcher(numero2));
+    }
+
+    /**
+     * Clase interna para manejar los cambios de texto en los EditText.
+     */
+    private class NumberTextWatcher implements TextWatcher {
+        /**
+         * EditText asociado a este TextWatcher.
+         */
+        private EditText editText;
+
+        /**
+         * Constructor de la clase.
+         *
+         * @param editText EditText asociado a este TextWatcher.
+         */
+        NumberTextWatcher(EditText editText) {
+            this.editText = editText;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        /**
+         * Este método se llama después de que el texto ha cambiado.
+         * Formatea el texto del EditText para mostrarlo con comas como separadores de miles.
+         *
+         * @param s El texto después del cambio.
+         */
+        @Override
+        public void afterTextChanged(Editable s) {
+            editText.removeTextChangedListener(this);
+
+            // Limpia el texto y lo formatea con comas como separadores de miles.
+            String cleanString = s.toString().replaceAll("[^\\d]", "");
+            if (!cleanString.isEmpty()) {
+                double parsed = Double.parseDouble(cleanString);
+                String formatted = df.format(parsed);
+
+                editText.setText(formatted);
+                editText.setSelection(formatted.length());
+            }
+
+            editText.addTextChangedListener(this);
+        }
+    }
+
+    /**
+     * Este método se llama cuando el usuario hace clic en el botón calcular.
+     * Realiza los cálculos necesarios y actualiza la interfaz de usuario con los resultados.
+     *
+     * @param view La vista que fue clickeada.
+     */
+    public void OnClick(View view){
+        // Obtiene los valores de los EditText y los limpia de comas.
+        String valor1 = numero1.getText().toString().replace(",", "");
+        String valor2 = numero2.getText().toString().replace(",", "");
+
+        // Realiza los cálculos si los valores no están vacíos.
+        if (!valor1.isEmpty() && !valor2.isEmpty()) {
+            int primerNumero = Integer.parseInt(valor1);
+            int segundoNumero = Integer.parseInt(valor2);
+            double calculo;
+
+            // Obtiene el RadioButton seleccionado y realiza el cálculo correspondiente.
+            int selectedId = radioGroup.getCheckedRadioButtonId();
+            RadioButton selectedRadioButton = findViewById(selectedId);
+            if (selectedRadioButton.getText().equals("Sin IVA")) {
+                calculo = (primerNumero + segundoNumero) * 1.19;
+            } else {
+                calculo = ((primerNumero/1.19) + segundoNumero) * 1.19;
+            }
+
+            // Formatea el resultado con comas como separadores de miles y lo muestra en el TextView.
+            NumberFormat nf = NumberFormat.getInstance(new Locale("es", "ES"));
+            String total = nf.format(calculo);
+            resultado.setText("$" + total);
+        }
+    }
+}
---
+++ app/src/main/java/org/beginningandroid/gananciaapp/SplashActivity.java
@@ -0,0 +1,47 @@
+package org.beginningandroid.gananciaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+/**
+ * La clase SplashActivity representa la pantalla de inicio de la aplicación.
+ * Esta clase se encarga de mostrar la versión de la aplicación y de iniciar la MainActivity después de un retraso.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    /**
+     * Este método se llama cuando se crea la actividad.
+     * Muestra la versión de la aplicación y inicia la MainActivity después de un retraso.
+     *
+     * @param savedInstanceState Si la actividad se reinicia después de una pausa o "stop", este es el último estado guardado.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // TextView para mostrar la versión de la aplicación.
+        TextView versionTextView = findViewById(R.id.textView2); // Asegúrate de usar el id correcto de tu TextView
+        try {
+            // Obtiene el nombre de la versión de la aplicación.
+            String versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+            // Muestra el nombre de la versión en el TextView.
+            versionTextView.setText("Versión " + versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Inicia la MainActivity después de un retraso.
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 800); // 800 = 0.8 segundos
+    }
+}
